9e178f8e5668cba5467894bf7a306fdafcb4b914
Ravikumar <pandu.ravi218@gmail.com>
2025-05-06 00:33:18 +0530

    module

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+    default = "ami-09c813fb71547fc4f"
+}
+
+variable "sg_id" {
+    
+}
+
+variable "instance_type" {
+    default = "t2.small"
+}
+
+variable "tags" {
+    default = {}
+}
